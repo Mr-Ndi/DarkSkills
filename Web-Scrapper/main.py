c9b3108637b30cdd08a8aa17aa9f796d4e863e18
Mr-Ndi <onlythenotes@gmail.com>
2025-06-07 14:43:05 +0200

    Done with forked shee

--- Web-Scrapper/main.py
+++ Web-Scrapper/main.py
@@ -5,5 +5,13 @@ with open('courses.html', 'r') as html_file :
     # print(content)
     soup = BeautifulSoup(content, 'lxml')
     # print(soup.prettify())
-    tag = soup.find_all('h5')
-    print(tag)
+    # tag = soup.find_all('h5')
+    # print(tag)
+    course_cards = soup.find_all('div',class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+
+        # print(course_name)
+        # print(course_price)
+        print("{} cost {}".format(course_name, course_price))
